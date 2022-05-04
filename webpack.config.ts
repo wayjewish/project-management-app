3d4a9ea2ccb8dff52707aa3ee937adaa852338e6
Dmitry Uvarov <diman_niks@mail.ru>
2022-05-05 01:38:55 +0300

    fix: webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,8 +4,6 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-import ESLintPlugin from 'eslint-webpack-plugin';
-
 import { Configuration as WebpackConfiguration } from "webpack";
 import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 
@@ -13,50 +11,59 @@ interface Configuration extends WebpackConfiguration {
   devServer?: WebpackDevServerConfiguration;
 }
 
-const config: Configuration = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+type CLIValues = boolean | string;
+type EnvValues = Record<string, CLIValues | Record<string, Env>>;
+interface Env extends EnvValues {}
+type Argv = Record<string, CLIValues>;
+interface WebpackConfigurationGenerator {
+  (env?: Env, argv?: Argv): Configuration;
+}
+
+const generateConfig: WebpackConfigurationGenerator = (env, argv) => {
+  return {
+    mode: argv && argv.mode === ("production" || "development" || "none") ? argv.mode : 'production',
+    entry: './src/index.tsx',
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader'],
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
-  },
-  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
-    template: './src/index.html',
-  })],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader'],
+        },
+      ],
     },
-    port: 3000,
-    compress: true,
-    historyApiFallback: true,
-    hot: true,
-  },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    },
+    plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, './src/assets'),
+      },
+      port: 3000,
+      compress: true,
+      open: true,
+    },
+  }
 };
 
-export default config;
+export default generateConfig;
