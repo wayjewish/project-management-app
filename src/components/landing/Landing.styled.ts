3c91126307719f1a13a820f4b6a95ef2f2007f46
Ekaterina <eka.kachan@gmail.com>
2022-05-11 18:39:02 +0400

    fix: fixed according to comment

--- src/components/landing/Landing.styled.ts
+++ src/components/landing/Landing.styled.ts
@@ -1,71 +1,26 @@
 import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-export const Main = styled('main')({
+export const HomePage = styled('main')({
   width: '100%',
   height: '100%',
   background: 'white',
 });
 
-export const Wrap = styled(Box)(({ theme }) => ({
-  flexWrap: 'wrap',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  margin: '36px 0px',
-
-  [theme.breakpoints.up('xs')]: {
-    padding: '0px 14px',
-  },
-  [theme.breakpoints.up('md')]: {
-    padding: '0px 25px',
-  },
-  [theme.breakpoints.up('lg')]: {
-    padding: '0px 120px',
-  },
-  [theme.breakpoints.up('xl')]: {
-    padding: '0px 360px',
-  },
-}));
-
-export const Content = styled(Box)({
-  display: 'flex',
-  flexWrap: 'wrap',
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  width: '100%',
-  marginBottom: '30px',
-});
-export const Image = styled('img')(({ theme }) => ({
-  [theme.breakpoints.up('xs')]: {
+export const HomeImage = styled('img')(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
     width: '0px',
-    height: '0px',
-  },
-  [theme.breakpoints.up('md')]: {
-    width: '290px',
-    height: '154px',
-  },
-  [theme.breakpoints.up('lg')]: {
-    width: '570px',
-    height: '245px',
   },
 }));
 
-export const BoxCards = styled('div')(({ theme }) => ({
+export const BoxCards = styled(Box)(({ theme }) => ({
   flexGrow: 1,
   marginBottom: '30px',
   [theme.breakpoints.down('md')]: {
     display: 'none',
   },
 }));
-export const Item = styled(Box)({
-  textAlign: 'left',
-  background: 'white',
-  boxShadow:
-    '0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12)',
-  borderRadius: '4px',
-  padding: '16px',
-});
+
 export const BoxVideo = styled(Box)({
   border: '1px solid #000000',
   borderRadius: '10px',
--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Box, Button, Grid, Typography } from '@mui/material';
-import { Main, Wrap, Content, Item, Image, BoxVideo, BoxCards } from './Landing.styled';
+import { Box, Button, Card, CardContent, Container, Grid, Typography } from '@mui/material';
+import { HomePage, HomeImage, BoxVideo, BoxCards } from './Landing.styled';
 import MainImg from '../../assets/img/main.svg';
 function Landing() {
   return (
-    <Main>
-      <Wrap>
-        <Content>
+    <HomePage>
+      <Container>
+        <Box
+          display={'flex'}
+          flexDirection={'row'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+          mb={3}
+        >
           <Box>
             <Typography variant="h2">RSS Tracker</Typography>
             <Typography variant="body1" mt={1} mb={1.6}>
@@ -19,65 +25,75 @@ function Landing() {
             </Button>
           </Box>
           <Box>
-            <Image src={MainImg} />
+            <HomeImage src={MainImg} />
           </Box>
-        </Content>
+        </Box>
         <BoxCards>
-          <Grid container spacing={3} style={{ justifyContent: 'center' }}>
-            <Grid item xs={6}>
-              <Item>
-                <Typography variant="body1" mb={1}>
-                  Вся рабочая информация всегда под рукой
-                </Typography>
-                <Typography variant="caption">
-                  Моментально оценивайте статус работы, определяйте дальнейшие действия и делайте
-                  всё необходимое, чтобы работа шла по плану.
-                </Typography>
-              </Item>
+          <Grid sx={{ flexGrow: 1 }} container spacing={3} gridAutoRows={200}>
+            <Grid item xs={6} container>
+              <Card variant="elevation" elevation={8}>
+                <CardContent>
+                  <Typography variant="body1" mb={1}>
+                    Вся рабочая информация всегда под рукой
+                  </Typography>
+                  <Typography variant="caption">
+                    Моментально оценивайте статус работы, определяйте дальнейшие действия и делайте
+                    всё необходимое, чтобы работа шла по плану.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
-            <Grid item xs={6}>
-              <Item>
-                <Typography variant="body1" mb={1}>
-                  Всё точно в срок
-                </Typography>
-                <Typography variant="caption">
-                  Создайте план, показывающий взаимосвязи между различными частями проекта. Он
-                  поможет вам работать по графику.
-                </Typography>
-              </Item>
+            <Grid item xs={6} container>
+              <Card variant="elevation" elevation={8}>
+                <CardContent>
+                  <Typography variant="body1" mb={1}>
+                    Всё точно в срок
+                  </Typography>
+                  <Typography variant="caption">
+                    Создайте план, показывающий взаимосвязи между различными частями проекта. Он
+                    поможет вам работать по графику.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
-            <Grid item xs={4}>
-              <Item>
-                <Typography variant="body1" mb={1}>
-                  Хронология
-                </Typography>
-                <Typography variant="caption">
-                  Управляйте зависящими друг от друга, пересекающимися и непредвиденными задачами, а
-                  также создавайте реалистичные планы.
-                </Typography>
-              </Item>
+            <Grid item xs={4} container>
+              <Card variant="elevation" elevation={8}>
+                <CardContent>
+                  <Typography variant="body1" mb={1}>
+                    Хронология
+                  </Typography>
+                  <Typography variant="caption">
+                    Управляйте зависящими друг от друга, пересекающимися и непредвиденными задачами,
+                    а также создавайте реалистичные планы.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
-            <Grid item xs={4}>
-              <Item>
-                <Typography variant="body1" mb={1}>
-                  Доски
-                </Typography>
-                <Typography variant="caption">
-                  Сделайте так, чтобы ничего не отвлекало сотрудников от текущих задач. Разбивайте
-                  работу на этапы, которые позволяют видеть, что важно, и где случаются задержки.
-                </Typography>
-              </Item>
+            <Grid item xs={4} container>
+              <Card variant="elevation" elevation={8}>
+                <CardContent>
+                  <Typography variant="body1" mb={1}>
+                    Доски
+                  </Typography>
+                  <Typography variant="caption">
+                    Сделайте так, чтобы ничего не отвлекало сотрудников от текущих задач. Разбивайте
+                    работу на этапы, которые позволяют видеть, что важно, и где случаются задержки.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
-            <Grid item xs={4}>
-              <Item>
-                <Typography variant="body1" mb={1}>
-                  Список
-                </Typography>
-                <Typography variant="caption">
-                  В списках сотрудники сразу видят, что нужно сделать, какие задачи приоритетны и
-                  какой срок отведён на выполнение работы.
-                </Typography>
-              </Item>
+            <Grid item xs={4} container>
+              <Card variant="elevation" elevation={8}>
+                <CardContent>
+                  <Typography variant="body1" mb={1}>
+                    Список
+                  </Typography>
+                  <Typography variant="caption">
+                    В списках сотрудники сразу видят, что нужно сделать, какие задачи приоритетны и
+                    какой срок отведён на выполнение работы.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
           </Grid>
         </BoxCards>
@@ -87,51 +103,57 @@ function Landing() {
           </Typography>
           <BoxVideo />
         </Box>
-        <BoxCards width={'100%'}>
+        <BoxCards>
           <Typography variant="h5" mb={1} textAlign={'left'}>
             Tracker подойдёт разным командам
           </Typography>
-          <Grid container spacing={3} style={{ justifyContent: 'center' }}>
+          <Grid container spacing={3} style={{ justifyContent: 'center' }} gridAutoRows={200}>
             <Grid item xs={4}>
-              <Item>
-                <Typography variant="body1" mb={1}>
-                  Разработка
-                </Typography>
-                <Typography variant="caption">
-                  Организуйте работу команды разработчиков в Tracker:
-                  оценивайте трудозатраты, планируйте спринты, контролируйте работу над задачами на
-                  виртуальной доске и следите за их выполнением на дашбордах.
-                </Typography>
-              </Item>
+              <Card variant="elevation" elevation={8}>
+                <CardContent>
+                  <Typography variant="body1" mb={1}>
+                    Разработка
+                  </Typography>
+                  <Typography variant="caption">
+                    Организуйте работу команды разработчиков в Tracker: оценивайте трудозатраты,
+                    планируйте спринты, контролируйте работу над задачами на виртуальной доске и
+                    следите за их выполнением на дашбордах.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
             <Grid item xs={4}>
-              <Item>
-                <Typography variant="body1" mb={1}>
-                  Дизайн
-                </Typography>
-                <Typography variant="caption">
-                  Используйте Tracker для итеративной работы. Обсуждайте макеты с коллегами прямо на
-                  странице задачи. А для типовых задач создавайте шаблоны ТЗ и автоматизируйте
-                  процесс согласования.
-                </Typography>
-              </Item>
+              <Card variant="elevation" elevation={8}>
+                <CardContent>
+                  <Typography variant="body1" mb={1}>
+                    Дизайн
+                  </Typography>
+                  <Typography variant="caption">
+                    Используйте Tracker для итеративной работы. Обсуждайте макеты с коллегами прямо
+                    на странице задачи. А для типовых задач создавайте шаблоны ТЗ и автоматизируйте
+                    процесс согласования.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
             <Grid item xs={4}>
-              <Item>
-                <Typography variant="body1" mb={1}>
-                  Инфраструктура
-                </Typography>
-                <Typography variant="caption">
-                  Решайте в Tracker инфраструктурные задачи, такие как закупка оборудования,
-                  инвентаризация, учёт расходов и другие.<br/> Чтобы контролировать процесс их
-                  реализации.
-                </Typography>
-              </Item>
+              <Card variant="elevation" elevation={8}>
+                <CardContent>
+                  <Typography variant="body1" mb={1}>
+                    Инфраструктура
+                  </Typography>
+                  <Typography variant="caption">
+                    Решайте в Tracker инфраструктурные задачи, такие как закупка оборудования,
+                    инвентаризация, учёт расходов и другие. Чтобы контролировать процесс их
+                    реализации.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
           </Grid>
         </BoxCards>
-      </Wrap>
-    </Main>
+      </Container>
+    </HomePage>
   );
 }
 
